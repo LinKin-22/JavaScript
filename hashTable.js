e9478bd9b310d0d8c6763c20d2b62298a85df43c
LinKin-22 <shomnathcse22@gmail.com>
2019-09-25 22:05:56 +0600

    created add function in hash table

--- hashTable.js
+++ hashTable.js
@@ -7,3 +7,32 @@ var hash = (string, max) => {
     }
     return hash%max;
 }
+
+let HashTable = function() {
+    let storage = [];
+    const storageLimit = 4;
+
+    this.print = function() {
+        console.log(storage);
+    }
+
+    this.add = function(key, value) {
+        var index = hash(key, storageLimit);
+        if(storage[index] === undefined) {
+            storage[index] = [
+                [key, value]
+            ];
+        } else {
+            var inserted = false;
+            for (let i = 0; i < storage.length; i++) {
+                if(storage[index][i][0] === key) {
+                    storage[index][i][1] = value;
+                    inserted = true;
+                }
+            }
+            if(inserted === false) {
+                storage[index].push([key, value]);
+            }
+        }
+    }
+}
