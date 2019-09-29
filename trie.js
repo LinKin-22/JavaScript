a639aa6618a3f5ed87f36acd475772a779f435c4
LinKin-22 <shomnathcse22@gmail.com>
2019-09-29 23:41:07 +0600

    added examples in trie

--- trie.js
+++ trie.js
@@ -61,3 +61,17 @@ let Trie = function() {
         return words.length > 0 ? words : null;
     }
 }
+
+let myTrie = new Trie();
+myTrie.add('ball');
+myTrie.add('bat');
+myTrie.add('doll');
+myTrie.add('dork');
+myTrie.add('do');
+myTrie.add('dorm');
+myTrie.add('send');
+myTrie.add('sense');
+console.log(myTrie.isWord('doll'));
+console.log(myTrie.isWord('dor'));
+console.log(myTrie.isWord('dorf'));
+console.log(myTrie.print());
