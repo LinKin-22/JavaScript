3f099a9bc3048245a920b96ffabc3b7fe8c2047f
LinKin-22 <shomnathcse22@gmail.com>
2019-09-29 23:33:53 +0600

    created print function in trie

--- trie.js
+++ trie.js
@@ -41,4 +41,23 @@ let Trie = function() {
         }
         return (node.keys.has(word) && node.keys.get(word).isEnd()) ? true : false;
     }
+
+    this.print = function() {
+        let words = new Array();
+        let search = function(node = this.root, string) {
+            if(node.keys.size != 0) {
+                for(let letter of node.keys.keys()) {
+                    search(node.keys.get(letter), string.concat(letter));
+                }
+                if(node.isEnd()) {
+                    words.push(string);
+                }
+            } else {
+                string.length > 0 ? words.push(string) : undefined;
+                return;
+            }
+        }
+        search(this.root, new String());
+        return words.length > 0 ? words : null;
+    }
 }
