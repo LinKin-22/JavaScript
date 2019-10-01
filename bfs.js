4a23dace63222054360ae762965709e360f9a560
LinKin-22 <shomnathcse22@gmail.com>
2019-10-02 00:17:34 +0600

    finished bfs of a graph

--- bfs.js
+++ bfs.js
@@ -16,6 +16,20 @@ function bfs(graph, cost) {
     while(queue.length !== 0) {
         current = queue.shift();
 
-        
+        var curConnected = graph[current];
+        var neighborIdx = curConnected.indexOf(1);
+
+        while(idx !== -1) {
+            neighborIdx.push(idx);
+            idx = curConnected.indexOf(1, idx+1);
+        }
+
+        for(var j=0; j < neighborIdx.length; j++) {
+            if(nodesLen[neighborIdx[j]] === Infinity) {
+                nodesLen[neighborIdx[j]] = nodesLen[current] + 1;
+                queue.push(neighborIdx[j]);
+            }
+        }
     }
+    return nodesLen;
 }
